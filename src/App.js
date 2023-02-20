f1be50f0c4e8345b7393e90cfe20513f9b9dfcc8
UmarAhmadxadjaev <azizhonahmadhadjaev@gmail.com>
2023-02-20 16:51:17 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+		<div className="main">
+		<div className='logo'>
+			<img src="logo.png"/>
+		</div>
+		
+		 <ul>
+		 <li className="active"><a href="#"><i className="fa fa-home"></i>Home</a></li>
+		 <li><a href="#">About</a></li>
+		 <li><a href="#">Dessert</a></li>
+		 <li><a href="#">Appetizers</a></li>
+		 <li><a href="#">Pasta</a></li>
+		 <li><a href="#">Salad</a></li>
+		 <li><a href="#">Burger</a></li>
+		 <li><a href="#">Pizza</a></li>
+		 <li><a href="#"><i className="fa fa-caret-down"></i> More Recipes <i className="fa fa-caret-down"></i></a>
+		 	<div className="sub-menu">
+		 		<ul>	 
+		 		 <li><a href="#">Cookies</a></li>
+		 		 <li><a href="#">Sandwich</a></li>
+		 	 </ul>
+		 	</div>
+		 </li>
+		</ul>
+
+		 </div>
+		 <div className="title">
+		 	<h1>HOMIE RECIPES</h1>
+		 </div>
+		 <div className="button">
+		 	<a href="#" className="btn">WATCH VIDEO</a>
+		 	<a href="#" className="btn">BASIC SKILLS</a>
+		 	<a href="#" className="btn">LEARN MORE</a>
+		 </div>
+
+		 	</header>
     </div>
   );
 }
